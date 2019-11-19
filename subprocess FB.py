32e88f756ccc1033ebe0bfc15d39f2b622efd3c4
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-11-19 11:07:06 +0100

    har lagt till fler men behöver beskrivas i koden
    pushar inom lek 1 om py

--- subprocess FB.py
+++ subprocess FB.py
@@ -2,13 +2,10 @@
 import subprocess
 import os
 
-cmd = "git --version"
+cmd = 'git --version'
 
 returned_value = os.system(cmd) #tillkalar git commando
-print("returned value:", returned_value)
+print('returned value:', returned_value)
 
-cmd = "date"
-
-returned_output = subprocess.check_output(cmd)
-
-print('Current date is:', returned_output.decode("utf-8"))
+shell = subprocess.run(['echo', 'hello world'], capture_output=True, shell=True)
+print(shell.stdout) 
