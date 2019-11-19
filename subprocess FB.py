8d5265b0ca0d1e634ef5e5eea5f54a64282f8294
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-11-19 09:59:47 +0100

    test
    Var = os.system_command ska nog få fram lista
    
    print("text", var [svarar med os command]
    
    var = subprocess.comman_second
    print("text", var.subproccess_output)
    
    ungeför ska det funka...

--- subprocess FB.py
+++ subprocess FB.py
@@ -1,1 +1,14 @@
 
+import subprocess
+import os
+
+cmd = "git --version"
+
+returned_value = os.system(cmd) #tillkalar git commando
+print("returned value:", returned_value)
+
+cmd = "date"
+
+returned_output = subprocess.check_output(cmd)
+
+print('Current date is:', returned_output.decode("utf-8"))
